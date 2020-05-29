a319c55f50d3445446c7dd923524636a389bfde8
Aswathi98 <aswathinair894@gmail.com>
2020-05-29 17:31:43 +0530

    Create login.ts

---
+++ login.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent  {
+  model: any = {};
+
+  onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model, null, 4));
+  }
+  
+
+}
